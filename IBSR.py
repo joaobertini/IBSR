d4f60141c2f41ad92032658ebe6a4eaf2c7acc82
bertiini <joaobertini@gmail.com>
2024-12-03 08:40:26 -0300

    primeira versão do IBSR

--- IBSR.py
+++ IBSR.py
@@ -14,19 +14,23 @@ from scipy.stats import poisson
 
 #______________
 class IBSRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, n_estimators=20, beta=0.5, theta=0.5, min_estimators = 10, learning_rate=0.1, scaler = 0.1):
+    def __init__(self, n_estimators=10, beta=0.5, theta=0.1, min_estimators = 10, learning_rate=1, scaler = 1.0):
         self.n_estimators = n_estimators
         self.min_estimators = min_estimators
         self.beta = beta
         self.theta = theta
+        self.buffer_X = []
+        self.buffer_y = []
+        self.window_size = 50
         self.learning_rate = learning_rate  # \lambda do boosting Oza
         self.scaler = scaler # \lambda para soma do ensemble Hastie
         self.regressors = []
         self.weights = []
         for _ in range(n_estimators):
-            regressor = tree.HoeffdingTreeRegressor(leaf_prediction='mean',max_depth=2) # depht = 1 stump #Hoeffding Adaptive Tree regressor (HATR)
+            regressor = tree.HoeffdingTreeRegressor(leaf_prediction='mean',max_depth=3) # depht = 1 stump #Hoeffding Adaptive Tree regressor (HATR)
             self.regressors.append(regressor) # adiciona regressor ao ensemble
             self.weights.append(1.0)
+
 
     def fit(self, X, y):
 
@@ -39,51 +43,180 @@ class IBSRegressor(BaseEstimator, RegressorMixin):
                     reg.learn_one(dict(enumerate(xi)), yi)
 
 
-    def partial_fit(self, X, y, L = 0):  # para um exemplo por vez
-        mu = 1
+    def partial_fit(self, X, y, L=0):
         residuo = y
         pred = 0
-        #for reg, wei in zip(self.regressors, self.weights): # reg in self.regressors:  # online boosting
-        for i in range(len(self.regressors)):
+        mu = 1
+        for i, regressor in enumerate(self.regressors):  # ver alternativa para não atualizar todos
             k = poisson.rvs(mu)
             for _ in range(k):
-                self.regressors[i].learn_one(dict(enumerate(X)),residuo)
+                regressor.learn_one(dict(enumerate(X)), residuo)
+            pred += self.scaler * regressor.predict_one(dict(enumerate(X)))
+            residuo -= pred
+            error = abs(residuo - pred)
+            #self.weights[i] = (1 - self.beta) * self.weights[i] + self.beta * error #(y / error)
+            mu = int(self.learning_rate * error)
+            # teste
+            if residuo < 0:
+                break
 
-            pred = pred + self.scaler*self.regressors[i].predict_one(dict(enumerate(X)))  # Hastie Eq. 8.10
-            residuo = residuo - pred                # eq. 8.11
-            error = abs(residuo - pred) # erro para determinar mu do proximo regressor
-            self.weights[i] = (1-self.beta)*self.weights[i] + self.beta*(y/error)  # y/pred
-            ## atualizar wei nos pesos
+        # if self.n_estimators > self.min_estimators:
+        #     try:
+        #         filtered = [(r, w) for r, w in zip(self.regressors, self.weights) if w > self.theta]
+        #         self.regressors, self.weights = map(list, zip(*filtered))
+        #         self.n_estimators = len(self.weights)
+        #     except ValueError:
+        #         print(f'Nenhum regressor tem peso maior que {self.theta}.')
 
-            mu = (int)(self.learning_rate*error)  # atualiza mu, ver valores
-            #print(mu)
-
-        # remove regressores -- ver se atualização de wei deve ser erro ou scaler*pred...
-        if self.n_estimators > self.min_estimators:                             #    elimina w < theta
-            try:  # verifiar se pode remover todos - deixar um aleatorio -- VER
-                filtered = [(r, w) for r, w in zip(self.regressors, self.weights) if w > self.theta]
-                self.regressors, self.weights = map(list, zip(*filtered))
-                # estava retornando em tupla
-                #self.regressors, self.weights = zip(*[(r, w) for r, w in zip(self.regressors, self.weights) if w > self.theta])
-
-                n_estimators = len(self.weights)
-            except ValueError as ve:
-                print(f'Nenhum regressor tem peso maior que {self.theta}.')
-
-            #errors = np.clip(errors, 1e-10, 1e10) # limitar valores nos erros
-        if L > 0:  # L é o número de regressores a adicionar
+        if L > 0:
             for _ in range(L):
-                regressor = tree.HoeffdingTreeRegressor(leaf_prediction='mean',max_depth=2) # depht = 1 stump #Hoeffding Adaptive Tree regressor (HATR)
-                self.regressors.append(regressor) # adiciona regressor ao ensemble
+                regressor = tree.HoeffdingTreeRegressor(leaf_prediction='mean', max_depth=3)
+                self.regressors.append(regressor)
                 self.weights.append(1.0)
-            self.n_estimators = len(self.weights)
+            self.n_estimators = len(self.regressors) #len(self.weights)
+
+
+
+    def partial_fit_Ens_old(self, X, y, p, L=0):  #p - ensemble predicton
+        residuo = y - p   # TESTES real - pred
+        mu = 1
+        for i, regressor in enumerate(self.regressors):  # ver alternativa para não atualizar todos - mais recentes
+            k = poisson.rvs(mu)
+            #print(k)
+            for _ in range(k):
+                regressor.learn_one(dict(enumerate(X)), residuo)
+            pred = self.scaler * regressor.predict_one(dict(enumerate(X)))
+            residuo = residuo - pred
 
 
     def get_n_estimators(self):
         return self.n_estimators
 
+    def partial_fit_Ens(self, X, y, p, L=0, erroLimiar= 2.0):  # p - ensemble prediction
+        residuo = y - p  # TESTES real - pred
+        mu = 1
+        percentUpdate = 10
 
-        # # Calcular e limitar o erro ponderado
+        if L == 0:
+            percentUpdate = 1
+            residuo = y
+
+        total_regressors = len(self.regressors)
+        num_recent_regressors = max(1, total_regressors // percentUpdate)  # Calcula os últimos 10%
+
+        # Itera sobre os últimos 10% dos regressores
+        for i, regressor in enumerate(self.regressors[-num_recent_regressors:]):
+            k = poisson.rvs(mu)
+            #print(k)
+            for _ in range(k):
+                regressor.learn_one(dict(enumerate(X)), residuo)
+            pred = self.scaler * regressor.predict_one(dict(enumerate(X)))
+            residuo = residuo - pred
+            error = abs(residuo - pred)  # wmape
+            #mu = int(self.learning_rate * error)
+
+        # Atualizar o buffer com a nova instância
+        self.buffer_X.append(X)
+        self.buffer_y.append(y)
+
+        # Manter apenas os últimos `window_size` elementos no buffer
+        if len(self.buffer_X) > self.window_size:
+            self.buffer_X.pop(0)
+            self.buffer_y.pop(0)
+
+        # Remover os regressores com erro maior que erroLimiar
+        #if self.n_estimators > 200: #len(self.buffer_X) == self.window_size:
+            #self.remove_least_contributing_regressors(self.buffer_X, self.buffer_y) #           self.remove_worst_regressors(erroLimiar)
+        #    self.remove_least_contributing_regressors_percent(self.buffer_X, self.buffer_y)
+
+        if L > 0:
+            for _ in range(L):
+                regressor = tree.HoeffdingTreeRegressor(leaf_prediction='mean', max_depth=2)
+                self.regressors.append(regressor)
+                self.weights.append(1.0)
+            self.n_estimators = len(self.regressors) #len(self.weights)
+
+
+    def remove_worst_regressors(self, erroLimiar):
+        # Inicializar lista de erros para cada regressor
+        wmape_errors = [0] * len(self.regressors)
+        X_window = self.buffer_X
+        y_window = self.buffer_y
+
+        # Calcular predições e WMAPE para cada regressor na janela
+        for j, regressor in enumerate(self.regressors):
+            preds = [self.scaler * regressor.predict_one(dict(enumerate(x))) for x in X_window]
+            absolute_errors = [abs(y_true - y_pred) for y_true, y_pred in zip(y_window, preds)]
+            wmape = sum(absolute_errors) / sum(y_window)
+            wmape_errors[j] += wmape
+
+        # Combinar regressores e erros WMAPE
+        regressor_errors = list(zip(self.regressors, wmape_errors))
+
+        # Filtrar os regressores com erro maior que erroLimiar
+        self.regressors = [regr for regr, err in regressor_errors if err <= erroLimiar]
+        self.weights = [1.0] * len(self.regressors)  # Atualizar pesos para novos regressores
+        print(max(wmape_errors))
+        print(len(wmape_errors) - len(self.regressors))
+
+
+    def remove_least_contributing_regressors(self, X, y): # X e y são de uma janela
+        total_error = np.sum(np.abs(y - self.predict_ensemble(X)))
+
+        # Calcular a contribuição de cada regressor
+        contributions = []
+        for i, regressor in enumerate(self.regressors):
+            # Predições sem o regressor atual
+            predictions_without_regressor = [
+                self.scaler * regressor.predict_one(dict(enumerate(x))) for idx, x in enumerate(X) if idx != i
+            ]
+            error_without_regressor = np.sum(np.abs(y - np.sum(predictions_without_regressor, axis=0)))
+            contribution = total_error - error_without_regressor
+            contributions.append((i, contribution))
+
+        # Ordenar os regressores pela contribuição (ascendente)
+        contributions.sort(key=lambda x: x[1])
+
+        # Número de regressores a remover (10% do total)
+        num_to_remove = max(1, len(self.regressors) // 10)
+
+        # Remover os regressores com menor contribuição
+        indices_to_remove = [idx for idx, _ in contributions[:num_to_remove]]
+        self.regressors = [regr for idx, regr in enumerate(self.regressors) if idx not in indices_to_remove]
+
+        print(f'Removidos {num_to_remove} regressores com menor contribuição.')
+
+
+
+    def predict_ensemble(self, X):
+    # Prever com todos o ensemble
+        return np.sum([self.scaler * regressor.predict_one(dict(enumerate(x))) for regressor in self.regressors for x in X], axis=0)
+
+
+    def remove_least_contributing_regressors_percent(self, X, y, threshold=0.05):
+        total_error = np.sum(np.abs(y - self.predict_ensemble(X)))
+
+        # Calcular a contribuição de cada regressor
+        contributions = []
+        for i, regressor in enumerate(self.regressors):
+            # Predições sem o regressor atual
+            predictions_without_regressor = [
+                self.scaler * regressor.predict_one(dict(enumerate(x))) for idx, x in enumerate(X) if idx != i
+            ]
+            error_without_regressor = np.sum(np.abs(y - np.sum(predictions_without_regressor, axis=0)))
+            contribution = (total_error - error_without_regressor) / total_error
+            contributions.append((i, contribution))
+
+        # Filtrar os regressores com contribuição inferior ao limiar
+        indices_to_remove = [idx for idx, contrib in contributions if contrib < threshold]
+        self.regressors = [regr for idx, regr in enumerate(self.regressors) if idx not in indices_to_remove]
+
+        print(f'Removidos {len(indices_to_remove)} regressores com contribuição inferior a {threshold*100}%.')
+
+
+
+
+    # # Calcular e limitar o erro ponderado
         # weighted_errors = [np.clip(np.mean(errors * w), 1e-10, 1e10) for w in self.weights]
         # self.weights = [(1 - self.beta) * w + self.beta * e for w, e in zip(self.weights, weighted_errors)]
         #
@@ -106,14 +239,64 @@ class IBSRegressor(BaseEstimator, RegressorMixin):
 
         # Adicionar novos regressores - batch ou online
 
-    def predict(self, X):
+    def predictTest(self, X):
+        """
+        Predicts target values for new data.
+        :param X: Feature matrix of shape (n_samples, n_features).
+        :return: Predicted target values.
+        """
+        y_pred = 0
+        for i, learner in enumerate(self.regressors):
+            y_pred += self.scaler * learner.predict_one(dict(enumerate(X)))
+        return y_pred
+
+    def predict1(self, X):
         if not self.regressors:
             raise Exception("Regressor não ajustado")
         if X.ndim == 1:     ## Ver mean, tentar alternativas como media ponderada com os pesos dos regressores
             predictions = np.sum([self.scaler*regressor.predict_one(dict(enumerate(X))) for regressor in self.regressors])
-        else:
+        else:   # era mean em sum
             predictions = np.sum([self.scaler*np.mean([regressor.predict_one(dict(enumerate(xi))) for regressor in self.regressors]) for xi in X])
         return predictions
+
+
+    def predict(self, X, y):
+        predictions = [self.scaler*regressor.predict_one(dict(enumerate(X))) for regressor in self.regressors]
+        boostPred = np.sum(predictions)
+        for i, (regressor, pred) in enumerate(zip(self.regressors, predictions)):
+            error = abs(y - pred)/y  # Calcula o erro absoluto
+            self.weights[i] = (1 - self.beta) * self.weights[i] + self.beta * abs(boostPred-y)/error  # Atualiza o peso para evitar divisão por zero
+
+        if self.n_estimators > self.min_estimators:
+            try:
+                filtered = [(r, w) for r, w in zip(self.regressors, self.weights) if w > self.theta]
+                self.regressors, self.weights = map(list, zip(*filtered))
+                self.n_estimators = len(self.weights)
+            except ValueError:
+                print(f'Nenhum regressor tem peso maior que {self.theta}.')
+
+        return boostPred
+
+    def predictAndUpdate(self, X,y):
+        predictions = [self.scaler*regressor.predict_one(dict(enumerate(X))) for regressor in self.regressors]
+        boostPred = np.sum(predictions)
+        EnsAbsSum = np.sum(np.abs(predictions))  # soma dos valores absolutos
+        for i, regressor in enumerate(self.regressors):
+            #if predictions[i]*y > 0 :
+                self.weights[i] = (1 - self.beta) * self.weights[i] + self.beta * np.abs(predictions[i])/EnsAbsSum  # Atualiza o peso para evitar divisão por zero
+            #else:
+            #    self.weights[i] = (1 - self.beta) * self.weights[i] + self.beta * np.abs(1/(1+predictions[i]))/EnsAbsSum
+
+        if self.n_estimators > self.min_estimators:
+            try:
+                filtered = [(r, w) for r, w in zip(self.regressors, self.weights) if w > self.theta]
+                self.regressors, self.weights = map(list, zip(*filtered))
+                self.n_estimators = len(self.weights)
+            except ValueError:
+                print(f'Nenhum regressor tem peso maior que {self.theta}.')
+
+        return boostPred
+
 
     # def prequential_evaluation(self, X, y, metric=mean_absolute_error):
     #     if len(X) != len(y):
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IBSR import IBSRegressor
+from SGDR import OnlineBoostingRegressor
+from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import make_friedman1, make_friedman2, make_friedman3
+
 
 from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeRegressor
@@ -28,21 +32,43 @@ def streamData(DATASET_PATH):
     #labels = values[:,-1].reshape(-1, 1)
 
 
+def streamDataArtificial():
+    housing = fetch_california_housing()
+    X = housing.data
+    y = housing.target
+
+    #X, y = make_friedman3(10000)
+    # Convertendo para DataFrame
+    df_X = pd.DataFrame(X)
+    df_y = pd.DataFrame(y)
+    # Combinando os dados
+    df = pd.concat([df_X, df_y], axis=1)
+    # Removendo os cabeçalhos
+    df.columns = range(df.shape[1])
+    values = df.values
+    # ensure all data is float >>> check if needed
+    # values = values.astype('float32')
+    return values
+    #features = values[:, :-1]
+    #labels = values[:,-1].reshape(-1, 1)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     DATASET_PATH = 'housing.csv'
 
-    ds = streamData(DATASET_PATH)
+    #ds = streamData(DATASET_PATH)
+    ds = streamDataArtificial()
 
     ibs =IBSRegressor()  # cria um modelo base
-    ibs.fit(ds[0,:-1],ds[0,-1])  # treina com o primeiro exemplo
+    ibs.partial_fit_Ens(ds[0,:-1],ds[0,-1],0) # fit(ds[0,:-1],ds[0,-1])  # treina com o primeiro exemplo
     ds = np.delete(ds, 0, axis=0)  # remove o exemplo usado
 
     performance_scores = [0]
     denominador = 1
     predictions = []
     prequential = []
+    onlineWMAPE=[]
     alpha = 0.999  # prequential forgetting parameter
     cont = 1 # conta exemplos processados para calculo do MAE
     erro = 0
@@ -54,13 +80,17 @@ if __name__ == '__main__':
     for row in ds:
 
         erroNominador = 0
+
         ##### testa ###
-        predictions.append(ibs.predict(row[:-1]))  # lista das predições
+        #predictions.append(ibs.predict(row[:-1]))  # lista das predições
+        #aux = ibs.predictTest(row[:-1]) #ibs.predict(row[:-1], row[-1]) # predicao - funcao que calcula weights  #
+        aux = ibs.predictAndUpdate(row[:-1],row[-1])
+        predictions.append(aux)
+        erro += abs(aux - row[-1])  # erro da instancia atual - MAE
 
         # prequential
-        erro += abs(ibs.predict(row[:-1]) - row[-1])  # erro da instancia atual - MAE
+        #erro += abs(ibs.predict(row[:-1]) - row[-1])  # erro da instancia atual - MAE
         ys += abs(row[-1]) # soma iterativa dos ys
-
 
         ## P(20000) = al20000 ...  al^3 x e1/al^3 + al^2 x e2/al^2 + al^1 x e3/al^1 + al^0 x e4/al^0
 
@@ -68,6 +98,7 @@ if __name__ == '__main__':
         #mae = mean_absolute_error(actual, pred)
         #wmape = mae * len(pred)*100/sum(abs(actual))
         errmae = erro/ys  # online wmape
+        onlineWMAPE.append(errmae)
 
         array = np.array(performance_scores) * alpha
         erroNominador = errmae + np.sum(array)
@@ -80,15 +111,24 @@ if __name__ == '__main__':
         cont += 1
 
         #calculo para adição de regressores
-        inner_term = math.exp(prequential[-1])
-        L = math.ceil(inner_term)
+        inner_term = math.exp(onlineWMAPE[-1])#/math.exp(0.2*onlineWMAPE[-1]) # math.exp(prequential[-1]) # /10
+        #inner_term = math.exp(prequential[-1])#/math.exp(0.5*prequential[-1]) # math.exp(prequential[-1]) # /10
+        L = math.ceil(inner_term) - 1
+        #L = math.floor(onlineWMAPE[-1]*10)-1
+        print(f"erro {onlineWMAPE[-1]} L {L}")
+        #if L > 20:
+        #    L = 20
 
-        # print(ibs.get_n_estimators()) # imprime tamanho do ensemble para teste
+        #print(ibs.get_n_estimators()) # imprime tamanho do ensemble para teste
         llist.append(ibs.get_n_estimators())
 
         ### treina ####
         # atualiza online - Poisson
-        ibs.partial_fit(row[:-1], row[-1],L)
+        if cont > 50:
+            ibs.partial_fit_Ens(row[:-1], row[-1], aux, L)
+        else:
+            ibs.partial_fit_Ens_old(row[:-1], row[-1],aux)
+
 
         # se erro > theta
         #adiciona n regressores a depender do erro - um a um ?
@@ -109,17 +149,38 @@ if __name__ == '__main__':
 
     fig1 = plt.figure(figsize=(6, 4))  # Figure for blue and red points
     fig2 = plt.figure(figsize=(6, 4))  # Figure for black points
+    fig3 = plt.figure(figsize=(6, 4))  # Figure for black points
+    fig4 = plt.figure(figsize=(6, 4))
+    fig5 = plt.figure(figsize=(6, 4))
     ax1 = fig1.add_subplot()
     ax2 = fig2.add_subplot()
+    ax3 = fig3.add_subplot()
+    ax4 = fig4.add_subplot()
+    ax5 = fig5.add_subplot()
     ax1.scatter(ax, ds[:,-1], s=1, c='blue', label='Actual')
     ax1.scatter(ax, predictions, s=0.5, c='red', label='Predicted')
     ax1.set_xlabel('Sample Stream', fontweight='bold')
     ax1.set_ylabel('y unit', fontweight='bold')
     ax1.legend()
     ax2.scatter(ax, prequential, s=0.5, c='black', label='Prequential error')
+    ax2.scatter(ax, onlineWMAPE, s=0.5, c='red', label='Online WMAPE')
     ax2.set_xlabel('Sample Stream', fontweight='bold')
     ax2.set_ylabel('y unit', fontweight='bold')
     ax2.legend()
+    ax3.scatter(ax, ds[:,-1] - predictions, s=0.5, c='black', label='Residuals')
+    ax3.plot([0.0,len(ds[:,-1] - predictions)],[0.0,0.0],'k')
+    ax3.set_xlabel('Sample Stream', fontweight='bold')
+    ax3.set_ylabel(r'y - $\hat{y}$', fontweight='bold')
+    ax3.legend()
+    ax4.scatter(predictions, ds[:,-1], s=1, c = 'blue')
+    ax4.plot([0.0,math.ceil(max(ds[:,-1]))],[0.0,math.ceil(max(ds[:,-1]))],'k')
+    ax4.set(xlabel='Predicted')
+    ax4.set(ylabel='Actual')
+    ax4.legend()
+    ax5.scatter(ax, llist, s=1, c = 'blue')
+    ax5.set(xlabel='Stream')
+    ax5.set(ylabel='Ensemble size')
+    ax5.legend()
     plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
